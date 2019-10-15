61c9545b161cdb5211c48c7b861cdaf41c9912b9
newbi462 <isthisemailfree@gmail.com>
2019-10-15 19:38:08 -0400

    Reteun order fix

--- assignments/closure.js
+++ assignments/closure.js
@@ -14,33 +14,36 @@ console.log(closureOfYourOwnCreation());
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-let count = 0;
-const counterMaker = () => {
+//let count = 0;
+let counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  //let count = 0; // I have to put this out side the funtion because of scope but instrutions say here?
-
+  let count = 0; // I have to put this out side the funtion because of scope but instrutions say here?
+return function() {
+  return ++count;
+}
   // 2- Declare a function `counter`. It should increment and return `count`.
-  function counter() {
-    count = count + 1;
-    return count; //?? is this what they mean?
-  };
+//  function counter() {
+//    count = count + 1;
+//    return count; //?? is this what they mean?
+//  };
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
 
   // 3- Return the `counter` function.
-  return counter();
+//  return counter();
 };
 
 // Example usage: const myCounter = counterMaker();
-//const myCounter = counterMaker();
+const myCounter = counterMaker();
 
 // myCounter(); // 1
-console.log(counterMaker());
+//console.log(counterMaker());
+console.log(myCounter());
 
 // myCounter(); // 2
-console.log(counterMaker());
-//console.log(myCounter);// wont let me call it as a funtion?
+//console.log(counterMaker());
+console.log(myCounter());// wont let me call it as a funtion?
 
 
 
