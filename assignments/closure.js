4d5a3f954c6beb150de4a15485d9b63728072fa5
newbi462 <isthisemailfree@gmail.com>
2019-10-15 16:41:55 -0400

    Closures + starting streatch

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,11 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+let name = "Russ";
+function closureOfYourOwnCreation() {
+  return "my name is " + name;
+};
+console.log(closureOfYourOwnCreation());
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -12,14 +17,24 @@
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+  let count = 0;
   // 2- Declare a function `counter`. It should increment and return `count`.
+  function counter() {
+    count = count + 1;
+    return count; //?? is this what they mean?
+  };
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+
+  return counter();
 };
 // Example usage: const myCounter = counterMaker();
+const myCounter = counterMaker();
 // myCounter(); // 1
+console.log(myCounter);
 // myCounter(); // 2
+console.log(myCounter);
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
